48aaf5d160789c687052ebc8011ecc8bdd8ddbd1
zzhdong <zzhdong@gmail.com>
2026-04-06 20:59:58 +0800

    fix carsh when pasting large text in TextView
    fix carsh when pasting large text in TextView

--- Sources/Runestone/TextView/TextSelection/CaretRectService.swift
+++ Sources/Runestone/TextView/TextSelection/CaretRectService.swift
@@ -30,14 +30,22 @@ final class CaretRectService {
         let safeLocation = min(max(location, 0), stringView.string.length)
         let line = lineManager.line(containingCharacterAt: safeLocation)!
         let lineController = lineControllerStorage.getOrCreateLineController(for: line)
-        let lineLocalLocation = safeLocation - line.location
-        if allowMovingCaretToNextLineFragment && shouldMoveCaretToNextLineFragment(forLocation: lineLocalLocation, in: line) {
-            let rect = caretRect(at: location + 1, allowMovingCaretToNextLineFragment: false)
+        let lineLocalLocation = min(max(safeLocation - line.location, 0), line.data.length)
+
+        if allowMovingCaretToNextLineFragment &&
+            shouldMoveCaretToNextLineFragment(forLocation: lineLocalLocation, in: line) {
+            let nextLocation = min(safeLocation + 1, stringView.string.length)
+            let rect = caretRect(at: nextLocation, allowMovingCaretToNextLineFragment: false)
             return CGRect(x: leadingLineSpacing, y: rect.minY, width: rect.width, height: rect.height)
         } else {
             let localCaretRect = lineController.caretRect(atIndex: lineLocalLocation)
             let globalYPosition = line.yPosition + localCaretRect.minY
-            let globalRect = CGRect(x: localCaretRect.minX, y: globalYPosition, width: localCaretRect.width, height: localCaretRect.height)
+            let globalRect = CGRect(
+                x: localCaretRect.minX,
+                y: globalYPosition,
+                width: localCaretRect.width,
+                height: localCaretRect.height
+            )
             return globalRect.offsetBy(dx: leadingLineSpacing, dy: textContainerInset.top)
         }
     }
@@ -49,12 +57,15 @@ private extension CaretRectService {
         guard lineController.numberOfLineFragments > 0 else {
             return false
         }
-        guard let lineFragmentNode = lineController.lineFragmentNode(containingCharacterAt: location) else {
+
+        let safeLocation = min(max(location, 0), line.data.length)
+
+        guard let lineFragmentNode = lineController.lineFragmentNode(containingCharacterAt: safeLocation) else {
             return false
         }
         guard lineFragmentNode.index > 0 else {
             return false
         }
-        return location == lineFragmentNode.data.lineFragment?.range.location
+        return safeLocation == lineFragmentNode.data.lineFragment?.range.location
     }
 }
