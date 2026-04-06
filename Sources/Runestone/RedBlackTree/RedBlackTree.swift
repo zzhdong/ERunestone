928ea4dac65e5801829b3017e35b82bb9c19c071
zzhdong <zzhdong@gmail.com>
2026-04-06 20:57:59 +0800

    fix carsh when pasting large text in TextView
    fix carsh when pasting large text in TextView

--- Sources/Runestone/RedBlackTree/RedBlackTree.swift
+++ Sources/Runestone/RedBlackTree/RedBlackTree.swift
@@ -34,16 +34,23 @@ final class RedBlackTree<NodeID: RedBlackTreeNodeID, NodeValue: RedBlackTreeNode
     }
 
     func node(containingLocation location: NodeValue) -> Node? {
-        guard location >= minimumValue && location <= root.nodeTotalValue else {
-            #if DEBUG
-            fatalError("\(location) is out of bounds. Valid range is \(minimumValue) - \(root.nodeTotalValue)."
-                       + " This issue is under investigation. Please open an issue at https://github.com/simonbs/Runestone/issues"
-                       + " and include this stack trace and a sample text file if possible. This fatal error is only thrown in debug builds.")
-            #else
-            return nil
-            #endif
+        let safeLocation = min(max(location, minimumValue), root.nodeTotalValue)
+    
+        #if DEBUG
+        if safeLocation != location {
+            assertionFailure(
+                "\(location) is out of bounds. Clamped to \(safeLocation). " +
+                "Valid range is \(minimumValue) - \(root.nodeTotalValue)."
+            )
         }
-        return node(containingLocation: location, minimumValue: minimumValue, valueKeyPath: \.value, totalValueKeyPath: \.nodeTotalValue)
+        #endif
+    
+        return node(
+            containingLocation: safeLocation,
+            minimumValue: minimumValue,
+            valueKeyPath: \.value,
+            totalValueKeyPath: \.nodeTotalValue
+        )
     }
 
     func node<T: Comparable & AdditiveArithmetic>(containingLocation location: T,
